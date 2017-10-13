5b8d37d498f51688eff36af2c2ec5fc59c416725
Kyzer <bridgeram@hotmail.com>
2017-10-12 22:15:44 -0500

    excercise 2
    12/10/2017

--- maze.js
+++ maze.js
@@ -1,15 +1,34 @@
 //Kyle Ramgolam 620076006
 window.onload = function () {
-    excercise1();
+    //excercise1();
+    excercise2();
 };
 
 
-function excercise1() 
-{
+function excercise1() {
+
     let Fboundary = document.getElementById("boundary1");
     
     Fboundary.onmouseover = function() {        
         this.setAttribute("class", "boundary youlose");        
     };
+
+}
+
+function excercise2() {
+
+	walls = document.querySelectorAll(".boundary");
+
+	for( var i=0; i<walls.length-1; i++ ){
+
+
+		walls[i].addEventListener("mouseover", function(){
+		 for (var i=0;i<walls.length-1;i++){
+
+			walls[i].setAttribute("class", "boundary youlose");}
+		});
+
+	}
+
 
 }
