69591861fba836a38dc03d2968936c4fd6100587
Kyzer <bridgeram@hotmail.com>
2017-10-12 21:27:13 -0500

    Finished Excercise 1

--- maze.js
+++ maze.js
@@ -1,1 +1,15 @@
-var hello = "hi";
+//Kyle Ramgolam 620076006
+window.onload = function () {
+  excercise1();
+};
+
+
+function excercise1() 
+{
+	let Fboundary = document.getElementById("boundary1");
+    
+	Fboundary.onmouseover = function() {        
+        this.setAttribute("class", "boundary youlose");        
+	};
+
+}
